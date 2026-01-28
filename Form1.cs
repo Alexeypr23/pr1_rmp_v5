4d6730ae6abae43a1732b143aca482df2cb743a7
mightykk7 <vladkobets0@gmail.com>
2026-01-28 15:03:24 +0500

    практическая 11.1

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pract11_Соколов_ПР_23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Ваше имя";
+            label2.Text = "Ваш рост";
+            label3.Text = "Ваш вес";
+            label4.Text = "Кол-во еды";
+            button1.Text = "Расчет веса";
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student stud = new Student();
+            stud.name = textBox1.Text;
+            stud.rost = Convert.ToInt32(numericUpDown1.Value);
+            stud.ves = Convert.ToInt32(numericUpDown2.Value);
+            stud.SetEat(Convert.ToDouble(numericUpDown3.Value));
+            label5.Text = string.Format($"Студент: {textBox1.Text}, Рост: {stud.rost} см, Вес: {stud.ves} кг", stud.name, stud.rost, stud.GetEat(0));
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract11_Соколов_ПР_23
+{
+    public class Student
+    {
+        public string name;
+        public int rost;
+        public double ves;
+
+        public void SetEat(double vs)
+        {
+            ves += vs;
+        }
+
+        public double GetEat(double vs)
+        {
+            double tempVes = vs;
+            return tempVes;
+        }
+    }
+
+    
+}
+
