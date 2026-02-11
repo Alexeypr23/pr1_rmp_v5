8efaac8a2d8bea9199b474076f41a4ff5fbee8bc
Alexeypr23 <olyxey8@gmail.com>
2026-02-11 14:56:54 +0500

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -8,29 +8,95 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace pract11_Соколов_ПР_23
+namespace pract12_v3_Соколов_ПР_23
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
-            label1.Text = "Ваше имя";
-            label2.Text = "Ваш рост";
-            label3.Text = "Ваш вес";
-            label4.Text = "Кол-во еды";
-            button1.Text = "Расчет веса";
         }
 
+        public class Sportsman
+        {
+            public string FirstName { get; set; }
+            public string Patronomyc { get; set; }
+            public string LastName { get; set; }
+            public string Gender { get; set; }
+            public int Age { get; set; }
+            public double Height { get; set; }
+            public double Weight { get; set; }
+            public string SportType { get; set; }
+
+            public string GetInfo()
+            {
+                return $"ФИО: {LastName} {FirstName} {Patronomyc}\nПол: {Gender}\nВозраст: {Age}\nВид спорта: {SportType}" ;
+            }
+
+            public double IdealWeightBroca()
+            {
+                if (Age <= 40)
+                {
+                    return Height - 110;
+                }
+                else
+                {
+                    return Height - 100;
+                }
+            }
+
+            public double IdealWeightCooper()
+            {
+                if (Gender == "Женский")
+                {
+                    return (Height * 3.5 - 108) * 0.453;
+                }
+                else
+                {
+                    if (Gender == "Мужской")
+                    {
+                        return (Height * 4.0 - 128) * 0.453;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+
+            public double CalculateBMI()
+            {
+                double heightMeters = Height / 100.0;
+                return Weight / (heightMeters * heightMeters);
+            }
+        }        
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Student stud = new Student();
-            stud.name = textBox1.Text;
-            stud.rost = Convert.ToInt32(numericUpDown1.Value);
-            stud.ves = Convert.ToInt32(numericUpDown2.Value);
-            stud.SetEat(Convert.ToDouble(numericUpDown3.Value));
-            label5.Text = string.Format($"Студент: {textBox1.Text}, Рост: {stud.rost} см, Вес: {stud.ves} кг", stud.name, stud.rost, stud.GetEat(0));
+            try
+            {
+                var sportsman = new Sportsman
+                {
+                    FirstName = textBox1.Text,
+                    Patronomyc = textBox2.Text,
+                    LastName = textBox3.Text,
+                    Age = Convert.ToInt32(textBox4.Text),
+                    Gender = comboBox1.Text,
+                    Height = Convert.ToDouble(textBox5.Text),
+                    Weight = Convert.ToDouble(textBox6.Text),
+                    SportType = textBox7.Text
+                };
+                string info = sportsman.GetInfo();
+                info += $"\n\nИдеаьный вес (формуа Брока):{sportsman.IdealWeightBroca()} кг";
+                info += $"\nИдеаьный вес (формуа Купера):{sportsman.IdealWeightCooper()} кг";
+                info += $"\nИМТ:{sportsman.CalculateBMI()}";
+                MessageBox.Show(info, "Информация о спортсмене");
+            }           
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка ввода данных" + ex.Message);
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,20 +1,19 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace pract11_Соколов_ПР_23
+namespace pract12_v3_Соколов_ПР_23
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
